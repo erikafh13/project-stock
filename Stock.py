9f1fcaba66efab3bf6ef3ba806572b192ad8799f
erikafh13 <127170590+erikafh13@users.noreply.github.com>
2025-08-05 15:23:35 +0700

    Update Stock.py

--- Stock.py
+++ Stock.py
@@ -104,8 +104,8 @@ def download_and_read(file_id, file_name, **kwargs):
 def read_produk_file(file_id):
     fh = download_file_from_gdrive(file_id)
     df = pd.read_excel(fh, sheet_name="Sheet1 (2)", skiprows=6, usecols=[0, 1, 2, 3])
-    # FIXED: Menstandarkan nama kolom langsung saat membaca file
-    df.columns = ['No. Barang', 'BRAND Barang', 'Kategori Barang', 'Nama Barang']
+    # FIXED: Menggunakan nama kolom asli dari file Excel untuk konsistensi
+    df.columns = ['No. Barang', 'BRAND Barang', 'Nama Kategori Barang Barang', 'Keterangan Barang']
     return df
 
 def read_stock_file(file_id):
@@ -274,7 +274,8 @@ elif page == "Hasil Analisa Stock":
     penjualan.rename(columns={'Qty': 'Kuantitas'}, inplace=True, errors='ignore')
     penjualan['Nama Dept'] = penjualan.apply(map_nama_dept, axis=1)
     penjualan['City'] = penjualan['Nama Dept'].apply(map_city)
-    # FIXED: Menghapus rename karena sudah distandarkan di read_produk_file
+    # FIXED: Menstandarkan penamaan kolom di awal
+    produk_ref.rename(columns={'Nama Kategori Barang Barang': 'Kategori Barang', 'Keterangan Barang': 'Nama Barang'}, inplace=True, errors='ignore')
     penjualan['Tgl Faktur'] = pd.to_datetime(penjualan['Tgl Faktur'], errors='coerce')
 
     st.header("Filter Analisis Stock")
