a77d1ad199e8007b16453be4ae4095911ea7144b
erikafh13 <127170590+erikafh13@users.noreply.github.com>
2025-09-10 09:35:50 +0700

    Update Stock.py

--- Stock.py
+++ Stock.py
@@ -58,7 +58,7 @@ try:
 
     if credentials:
         drive_service = build('drive', 'v3', credentials=credentials)
-        folder_penjualan = "1Okgw8qHVM8HyBwnTUFHbmYkNKqCcswNZ"
+        folder_penjualan = "1wH9o4dyNfjve9ScJ_DB2TwT0EDsPe9Zf"
         folder_produk = "1UdGbFzZ2Wv83YZLNwdU-rgY-LXlczsFv"
         folder_stock = "1PMeH_wvgRUnyiZyZ_wrmKAATX9JyWzq_"
         DRIVE_AVAILABLE = True
@@ -323,6 +323,7 @@ elif page == "Hasil Analisa ROP":
             file_name=f"Hasil_Analisis_ROP_{start_date}_sd_{end_date}.xlsx",
             mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
         )
+
 
 
 
